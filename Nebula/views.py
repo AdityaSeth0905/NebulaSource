18cd174677de0f831585edb9feec03760e7329e6
Aditya seth <adityaseth936@gmail.com>
2024-06-05 00:43:37 +0530

    sending code

---
+++ Nebula/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def stylesheets(request):
+    return render(request, 'indexpage.css')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Query MongoDB for user record
+       # user = User.objects.filter(username=username).first()
+
+        if user:
+            # Compare passwords
+            if user.check_password(password):
+                # Authenticate user
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    # Redirect to dashboard or another page
+                    return redirect('dashboard')
+        # Handle invalid credentials
+        return render(request, 'login.html', {'error_message': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
+
